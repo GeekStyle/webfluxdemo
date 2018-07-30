5ff8c1adfd4010ac8d5c84b2633239be9fd9e103
wei.yang <geekstylecn@gmail.com>
2018-07-30 10:14:57 +0800

    sync

--- src/main/java/com/geekstyle/springwebfluxdemo/controller/TestController.java
+++ src/main/java/com/geekstyle/springwebfluxdemo/controller/TestController.java
@@ -1,6 +1,7 @@
 package com.geekstyle.springwebfluxdemo.controller;
 
 import java.time.Duration;
+import java.util.concurrent.TimeUnit;
 
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -11,10 +12,20 @@ import reactor.core.publisher.Mono;
 @RestController
 public class TestController {
 	
+//	 @GetMapping("/hello/{latency}")
+//     public Mono<String> hello(@PathVariable int latency) {
+//         return Mono.just("Welcome to reactive world ~")
+//                 .delayElement(Duration.ofMillis(latency)); // 1
+//     }
+	 
 	 @GetMapping("/hello/{latency}")
-     public Mono<String> hello(@PathVariable int latency) {
-         return Mono.just("Welcome to reactive world ~")
-                 .delayElement(Duration.ofMillis(latency)); // 1
+     public Mono<String> hello(@PathVariable long latency) {
+         try {
+             TimeUnit.MILLISECONDS.sleep(latency);   // 1
+         } catch (InterruptedException e) {
+             return Mono.just("Error during thread sleep");
+         }
+         return Mono.just("Welcome to reactive world ~");
      }
-	
+	 
 }
